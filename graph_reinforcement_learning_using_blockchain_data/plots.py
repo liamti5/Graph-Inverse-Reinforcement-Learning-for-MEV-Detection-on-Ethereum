e2ab66298e28e379949fb4e49e6e75fb6380d637
Liam Tessendorf <liam.tessendorf@uzh.ch>
2025-03-30 11:47:36 +0200

    Add histogram for plotting len labels

--- graph_reinforcement_learning_using_blockchain_data/plots.py
+++ graph_reinforcement_learning_using_blockchain_data/plots.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import typer
 from loguru import logger
 from tqdm import tqdm
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pandas as pd
 
 from graph_reinforcement_learning_using_blockchain_data.config import (
     FIGURES_DIR,
@@ -10,6 +13,29 @@ from graph_reinforcement_learning_using_blockchain_data.config import (
 )
 
 app = typer.Typer()
+
+def plot_hist(df, label_name):
+    sns.set(style="whitegrid")
+
+    plt.figure(figsize=(8, 6))
+    ax = sns.histplot(df[label_name], bins=2, discrete=True)
+
+    for p in ax.patches:
+        ax.annotate(
+            f"{int(p.get_height())}",
+            (p.get_x() + p.get_width() / 2.0, p.get_height()),
+            ha="center",
+            va="center",
+            xytext=(0, 10),
+            textcoords="offset points",
+        )
+
+    ax.set_xticks([0, 1])
+    ax.set_xlabel("Class Label")
+    ax.set_ylabel("Count")
+    ax.set_title("Histogram of Class Labels")
+
+    plt.show()
 
 
 @app.command()
