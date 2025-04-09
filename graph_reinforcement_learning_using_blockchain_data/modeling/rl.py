fe409c68840398324a951f0061a292d4caf896a4
Liam Tessendorf <liam.tessendorf@uzh.ch>
2025-04-09 14:26:25 +0200

    Use MLflowOutputFormat from utils

--- graph_reinforcement_learning_using_blockchain_data/modeling/rl.py
+++ graph_reinforcement_learning_using_blockchain_data/modeling/rl.py
@@ -12,11 +12,16 @@ from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-from graph_reinforcement_learning_using_blockchain_data import config
+from graph_reinforcement_learning_using_blockchain_data import config, utils
 
 mlflow.set_tracking_uri(uri=config.MLFLOW_TRACKING_URI)
 
 np.random.seed(42)
+
+loggers = Logger(
+    folder=None,
+    output_formats=[HumanOutputFormat(sys.stdout), utils.MLflowOutputFormat()],
+)
 
 
 class BlockchainEnv(gym.Env):
@@ -186,36 +191,6 @@ class BlockchainEnv(gym.Env):
 #         # Critic returns state-value estimates for each address.
 #         values = self.critic(states)  # shape: (max_addresses, 1)
 #         return actions, log_probs, entropy, values
-
-
-class MLflowOutputFormat(KVWriter):
-    """
-    Dumps key/value pairs into MLflow's numeric format.
-    """
-
-    def write(
-        self,
-        key_values: Dict[str, Any],
-        key_excluded: Dict[str, Union[str, Tuple[str, ...]]],
-        step: int = 0,
-    ) -> None:
-
-        for (key, value), (_, excluded) in zip(
-            sorted(key_values.items()), sorted(key_excluded.items())
-        ):
-
-            if excluded is not None and "mlflow" in excluded:
-                continue
-
-            if isinstance(value, np.ScalarType):
-                if not isinstance(value, str):
-                    mlflow.log_metric(key, value, step)
-
-
-loggers = Logger(
-    folder=None,
-    output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
-)
 
 
 def run(
